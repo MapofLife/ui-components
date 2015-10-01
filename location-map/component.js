3dda05e690e7a08b300576c51a315608e942e5a4
Ajay Ranipeta <ajay.ranipeta@gmail.com>
2015-09-30 23:36:24 -0400

    Draw regions based on region id

--- location-map/component.js
+++ location-map/component.js
@@ -1,7 +1,7 @@
 angular.module('mol.location-map',['mol-location-map-templates'])
   .directive('molLocationMap', [
-    '$state','$http','leafletData',
-    function($state,$http, leafletData) {
+    'MOLApiX','$state','$http','leafletData', 'leafletBoundsHelpers',
+    function(MOLApiX, $state,$http, leafletData, leafletBoundsHelpers) {
 
     return {
       restrict: 'AE',
@@ -15,7 +15,7 @@ angular.module('mol.location-map',['mol-location-map-templates'])
 
 
       },
-      controller: function($scope,$state, $http, $element, leafletData) {
+      controller: function($scope,$state, $http, $element, leafletData, MOLApiX) {
 
         console.log($element);
         addResizeListener($element.context,
@@ -38,6 +38,8 @@ angular.module('mol.location-map',['mol-location-map-templates'])
               };
         $scope.paths = {};
         $scope.location = {};
+        $scope.region = {};
+        $scope.bounds = {};
         $scope.layers = {
           baselayers: {
             positron: {
@@ -58,10 +60,12 @@ angular.module('mol.location-map',['mol-location-map-templates'])
                   name: 'Shapes',
                   type: 'group',
                   visible: true
+              },
+              region: {
+                  visible: false
               }
           }
         };
-
 
         $scope.setLocation = function(location) {
           $scope.location = {
@@ -69,7 +73,21 @@ angular.module('mol.location-map',['mol-location-map-templates'])
             lng: parseFloat(location.lng),
             type: 'point'
           }
-        }
+        };
+
+        $scope.setRegion = function(region) {
+            var theBounds = leafletBoundsHelpers.createBoundsFromArray([
+                [region.extent.coordinates[0][2][1], region.extent.coordinates[0][2][0]],
+                [region.extent.coordinates[0][0][1], region.extent.coordinates[0][0][0]]
+            ]);
+            $scope.region = {
+                regionName: region.region_name,
+                regionType: region.region_type,
+                regionId: region.region_id,
+                bounds: theBounds,
+                type: 'polygon'
+            };
+        };
 
         $scope.$watch(
           'location',
@@ -99,10 +117,57 @@ angular.module('mol.location-map',['mol-location-map-templates'])
                     radius: 50000,
                     layer: 'shapes'
               };
+
+              $scope.layers.overlays.shapes = {
+                  visible: true
+              };
+              $scope.layers.overlays.region = {
+                visible: false
+              };
+
             }
           },true
         );
 
+          $scope.$watch(
+              'region',
+              function(region, oldValue) {
+                  if (region.regionId !== undefined) {
+                      $http({
+                          "withCredentials": false,
+                          "method": "POST",
+                          "url": "https://mol.cartodb.com/api/v1/map/named/display_region",
+                          "data": {
+                              "regionid": region.regionId
+                          }
+                      }).then(
+                          function(result, status, headers, config) {
+                              console.log(result);
+                              $scope.layers.overlays.region = {
+                                  type: "xyz",
+                                  url: "//d3dvrpov25vfw0.cloudfront.net/api/v1/map/{0}/{z}/{x}/{y}.png"
+                                      .format(result.data.layergroupid),
+                                  layerOptions: {
+                                      attribution: '©2015 Map of Life',
+                                      continuousWorld: false
+                                  },
+                                  name: 'display_region',
+                                  opacity: 0.8,
+                                  refresh: true,
+                                  doRefresh: true,
+                                  errorTileUrl: '/app/img/blank_tile.png',
+                                  visible: true
+                              };
+
+                              $scope.layers.overlays.shapes = {
+                                  visible: false
+                              };
+                              $scope.bounds = $scope.region.bounds;
+                          }
+                      );
+                  }
+              },true
+          );
 
        $scope.$on("leafletDirectiveMap.click", function(event, args){
            var leafEvent = args.leafletEvent;
@@ -117,25 +182,36 @@ angular.module('mol.location-map',['mol-location-map-templates'])
        if($state.params.lat && $state.params.lng) {
          $scope.setLocation($state.params);
        } else if ($state.params.placename) {
-         $http({
-             url: 'http://nominatim.openstreetmap.org/search',
-             method: 'GET',
-             params: {
-               q: $state.params.placename.replace(/_/g,''),
-               format: 'json'
-             },
-             withCredentials: false
-         }).then(
-           function(response) {
-             try{
-               var location = {
-                 lat: response.data[0].lat,
-                 lng: response.data[0].lon
-              };
-               $scope.setLocation(location);
-             } catch(e) {}
+
+           var placename = $state.params.placename;
+           if (placename == parseInt(placename)) {
+              MOLApiX('searchregion', {regionid: placename}, '0.x')
+                  .then(
+                  function(response) {
+                      $scope.setRegion(response.data[0]);
+                  }
+              );
+           } else {
+             $http({
+                 url: 'http://nominatim.openstreetmap.org/search',
+                 method: 'GET',
+                 params: {
+                   q: placename.replace(/_/g,''),
+                   format: 'json'
+                 },
+                 withCredentials: false
+             }).then(
+               function(response) {
+                 try{
+                   var location = {
+                     lat: response.data[0].lat,
+                     lng: response.data[0].lon
+                  };
+                   $scope.setLocation(location);
+                 } catch(e) {}
+               }
+             );
            }
-         );
        }
 
       }
--- location-map/templates.js
+++ location-map/templates.js
@@ -1,4 +1,4 @@
 angular.module('mol-location-map-templates', []).run(['$templateCache', function($templateCache) {
   $templateCache.put("mol-location-map-main.html",
-    "<leaflet paths=paths markers=markers center=center width={{width}} layers=layers defaults=defaults height={{height}}></leaflet>");
+    "<leaflet paths=paths markers=markers center=center bounds=bounds width={{width}} layers=layers defaults=defaults height={{height}}></leaflet>");
 }]);
