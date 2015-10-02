31676734ed26e4891116c55f7c0c9720ee03c252
Jeremy Malczyk <jmalczyk@gmail.com>
2015-10-02 17:34:17 -0400

    Fixing species-list-service

--- taxa-counts/component.js
+++ taxa-counts/component.js
@@ -9,9 +9,8 @@ angular.module('mol.taxa-counts',['mol-taxa-counts-templates'])
       },
       templateUrl: 'mol-taxa-counts-main.html',
       controller: function($scope,$filter) {
+
         $scope.groupList = undefined;
-
-
         $scope.$watch(
           function($scope){
             return [$scope.taxa,$scope.filterBy];
@@ -19,10 +18,21 @@ angular.module('mol.taxa-counts',['mol-taxa-counts-templates'])
           function(newValues, oldValues) {
               $scope.groupList = $filter('taxa')(
                 angular.copy(newValues[0]), newValues[1]);
+              if($scope.taxon) {
+                angular.forEach(
+                  $scope.groupList,
+                  function(group) {
+                    if(group.taxa === $scope.taxon.taxa) {
+                      $scope.selectTaxon(group);
+                    }
+                  }
+                )
+              }
             },true
         );
 
         $scope.selectTaxon = function(taxon) {
+          $scope.selected = taxon.taxa
           $scope.taxon = taxon;
         }
       }
