5b070596209da3082ae4253d9922e137e5e3273a
Ajay Ranipeta <ajay.ranipeta@gmail.com>
2017-02-23 00:38:15 -0500

    Make species images actually work and add full-size option

--- species-images/component.js
+++ species-images/component.js
@@ -1,7 +1,53 @@
-angular.module('mol.species-images',['mol-species-images-templates'])
+angular.module('mol.species-images', ['bootstrapLightbox', 'mol-species-images-templates'])
+  .config(function (LightboxProvider) {
+    
+    LightboxProvider.fullScreenMode = true;
+    LightboxProvider.templateUrl = 'mol-species-images-modal.html';
+
+    LightboxProvider.getImageUrl = function (image) {
+      var url = image.asset_url || image.url;
+      url = (url.includes('=s')) ? url.substring(0, url.indexOf('=s')) : url;
+      return url;
+    };
+
+    LightboxProvider.getImageCaption = function (image) {
+      var rights = "";
+      
+      if ( isValid(image.copyright) || isValid(image.license) ) {
+        var lic = "";
+        if (isValid(image.license)) {
+          var pos = image.license.indexOf("creativecommons.org");
+          if (pos > 0) {
+            pos += 29;
+            lic = "CC " + image.license.substring(pos);
+            lic = lic.replace(/\//g, ' ').toUpperCase();
+          } 
+        }
+
+        rights = (isValid(image.copyright))?image.copyright : "";
+        if (isValid(rights) && isValid(lic)) {
+          rights = "\u00A9 " + rights + " / ";
+        }
+        rights += lic;
+      }
+
+      return rights;
+    };        
+
+    function isValid(str) {
+      if (str) {
+        if (typeof (str) == "string") {
+          if (str.length > 0) {
+            return true;
+          }
+        }
+      }
+      return false;
+    }
+  })
 .directive('molSpeciesImages', [
-  '$state','$q','$timeout','$cookies','molApi',
-  function($state,$q, $timeout,$cookies, molApi) {
+  '$state', '$q', '$timeout', '$cookies', 'molApi', 'Lightbox', 
+  function ($state, $q, $timeout, $cookies, molApi, Lightbox) {
 
     return {
       restrict: 'E',
@@ -14,7 +60,7 @@ angular.module('mol.species-images',['mol-species-images-templates'])
       controller: function($scope,$q, $state,$timeout,$cookies) {
         $scope.canceller = $q.defer();
         $scope.images = undefined;
-        $scope.loggedIn = ($cookies.muprsns !== undefined);
+        $scope.loggedIn = ($cookies.get('muprsns') !== undefined);
         $scope.$watch(
           'scientificname',
           function(n,o) {
@@ -44,7 +90,7 @@ angular.module('mol.species-images',['mol-species-images-templates'])
           //make api call for vote@asset_id
           $scope.scrolling = false;
           var currVote, newVote;
-          if($cookies.muprsns) {
+          if ($cookies.get('muprsns')) {
             currVote = $scope.images[$scope.selectedImage]["vote"];
             if( currVote == vote) {
               newVote = 0;
@@ -52,16 +98,18 @@ angular.module('mol.species-images',['mol-species-images-templates'])
               newVote = vote;
             }
             $scope.images[$scope.selectedImage]["vote"] = newVote;
-            $http.get(
-              'https://auth.mol.org/contribute/asset/{0}/vote/{1}'
-                .format($scope.images[$scope.selectedImage]["asset_id"],newVote)
-            ).success(
-              function(response) {
-                if (response.status == "success" && response.asset_id == $scope.images[$scope.selectedImage]["asset_id"]) {
-                  $scope.images[$scope.selectedImage]["vote"] = response.vote_type;
+            molApi({
+              "service": "species/images/vote",
+              "params": { "asset_id": $scope.images[$scope.selectedImage]["asset_id"], "vote_value": newVote}
+            }).then(
+              function (response) {
+                if (response.status == "success" 
+                    && response.data 
+                    && response.data.length > 0 
+                    && response.asset_id == $scope.images[$scope.selectedImage]["asset_id"]) {
+                  $scope.images[$scope.selectedImage]["vote"] = response.data[0].vote_type;
                 }
-              }
-            );
+              });            
           }
         }
         $scope.scrollImage = function(increment, autoscroll) {
@@ -78,6 +126,10 @@ angular.module('mol.species-images',['mol-species-images-templates'])
           if($scope.scrolling) {
             $timeout(function(){$scope.scrollImage(1, true)},5000);
           }
+        }
+
+        $scope.fullsizeImage = function (selectedImage) {
+          Lightbox.openModal($scope.images, selectedImage, { scope: $scope });
         }
       }
     };
