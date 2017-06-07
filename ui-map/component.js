1497d9915c67540003a5b8b91e8da38868c2f43f
Ajay Ranipeta <ajay.ranipeta@gmail.com>
2017-06-07 10:36:46 -0400

    Add JSONP checks for Angular 1.6+

--- ui-map/component.js
+++ ui-map/component.js
@@ -60,17 +60,26 @@ angular.module('mol.ui-map', ['uiGmapgoogle-maps'])
 												if(!grid[z]) {grid[z]={}};
 												if(!grid[z][c.x]) {grid[z][c.x]={}};
 												if(!grid[z][c.x][c.y]) {
-													$http({
-														url:'{0}?callback=JSON_CALLBACK'.format(grid_url),
-														method: 'JSONP',
-														ignoreLoadingbar:true
-													}).success(
-															function(data, status, headers, config) {
+													
+													// JSONP calls based on AngularJS version
+													if (angular.version.major == 1 && angular.version.minor < 6) {
+														$http({
+															url: '{0}?callback=JSON_CALLBACK'.format(grid_url),
+															method: 'JSONP',
+															ignoreLoadingbar: true
+														}).success(
+															function (data, status, headers, config) {
 																grid[z][c.x][c.y] = data;
 															}
-														).error(function(err) {});
+															).error(function (err) { });
+													} else {
+														$http.jsonp(grid_url, { jsonpCallbackParam: 'callback' })
+														.success(function (data) {
+															grid[z][c.x][c.y] = data;
+														});
 													}
 												}
+											}
 										}
 									} catch (e){}
 
