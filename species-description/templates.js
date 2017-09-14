434b109b176c42975fc0fa61393f86847505e183
Ajay Ranipeta <ajay.ranipeta@gmail.com>
2017-09-14 13:47:30 -0400

    Update for translation

--- species-description/templates.js
+++ species-description/templates.js
@@ -1,4 +1,4 @@
-angular.module('mol-species-description-templates', []).run(['$templateCache', function($templateCache) {
+angular.module('mol-species-description-templates', []).run(['$templateCache', function ($templateCache) {
   $templateCache.put("mol-species-description-main.html",
-    "{{model.content}} <span ng-show=model.content>Source: <a ng-href=\"http://wikipedia.org/wiki/{{scientificname.replace(' ','_')}}\">Wikipedia</a></span>");
+    "{{model.content}} <span ng-show=model.content class=pull-right><br><span translate>source</span>: <a ng-href=\"http://{{model.lang}}.wikipedia.org/wiki/{{scientificname.replace(' ','_')}}\" target=_blank>Wikipedia</a></span>");
 }]);
